8dc4e3cb51427b73ababa70af3b4fea93b89f87b
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-01-20 17:52:44 +0100

    get current letters

--- main/script.js
+++ main/script.js
@@ -1,18 +1,29 @@
-console.log('hello world');
 const lesson = document.querySelector('#lesson');
 const input = document.querySelector('#input');
+
 input.addEventListener('input', symbolTyped);
 
+let letterIndex = 0;
 function symbolTyped(){
-    const symbol = input.value;
-    show(symbol);
-}
 
+    const symbol = getLastLetter(input.value);
+    const target = getCurrentLetter();
+    show(target);
+    letterIndex++;
+}
 
 
 
 function show(text){
     const display = document.querySelector('#display');
     display.innerHTML = text;
+}
+
+function getLastLetter(text){
+    return text.charAt(text.length - 1);
+}
+
+function getCurrentLetter(){
+    return lesson.innerHTML[letterIndex];
 }
 
